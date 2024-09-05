79c077d917e176745b305d1723c79911c816ab64
lancer <virajsingh4553@gmail.com>
2024-09-05 17:08:05 +0530

    finall

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 
 const express = require('express');
@@ -11,10 +12,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoURI = process.env.MONGO_URI;
+
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongoURI', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
