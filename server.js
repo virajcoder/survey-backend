b3d2a2d9581076fdfd083c4740c0e63ca04db293
lancer <virajsingh4553@gmail.com>
2024-09-05 12:39:53 +0530

    url commite

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const port = 5000;
+
 
 
 app.use(cors());
 app.use(bodyParser.json());
 
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/survey', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongoURI', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
